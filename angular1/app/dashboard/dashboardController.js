a5091a9ca3c5c8b60c4606d339023542becad4ab
Douglas Carvalho <douglaspdc@gmail.com>
2017-03-29 14:32:07 -0300

    add IIFE and controller style from john papa guide

--- angular1/app/dashboard/dashboardController.js
+++ angular1/app/dashboard/dashboardController.js
@@ -1,22 +1,22 @@
-angular.module('primeiraApp').controller('DashBoardCtrl', [
-  '$scope',
-  '$http',
-  DashboardController
-])
+(function() {
+  angular.module('primeiraApp').controller('DashBoardCtrl', [
+    '$http',
+    DashboardController
+  ])
 
-function DashboardController($scope, $http) {
+  function DashboardController($http) {
+    const vm = this
+    vm.getSummary = function() {
+      const url = 'http://localhost:3003/api/billingSummary'
+      $http.get(url).then(function(response) {
 
-  $scope.getSummary = function() {
-    const url = 'http://localhost:3003/api/billingSummary'
-    $http.get(url).then(function(response) {
+        const {credit = 0, debt = 0} = response.data
+        vm.credit = credit
+        vm.debt = debt
+        vm.total = credit - debt
+      })
+    }
 
-      const {credit = 0, debt = 0} = response.data
-      $scope.credit = credit
-      $scope.debt = debt
-      $scope.total = credit - debt
-    })
+    vm.getSummary()
   }
-
-  $scope.getSummary()
-
-}
+})()
